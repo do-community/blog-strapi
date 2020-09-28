cd0a7aacc3490296aba73a813909b8e8eefff6b1
Chris on Code <hey@chrisoncode.io>
2020-09-28 12:23:40 -0700

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+# Strapi application
+
+A quick description of your strapi application
+
+## Instructions to Deploy to App Platform
+
+1. Setup database connections in `config/database.js`
+2. Set public URL based in `config/server.js`
---
+++ config/env/production/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {},
+    },
+  },
+});
