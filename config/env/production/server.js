121898f3270453e30f449c5071be8ed3f99f786c
Chris on Code <hey@chrisoncode.io>
2020-09-29 09:13:54 -0700

    adding admin stuff to server config?

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   url: env("STRAPI_URL"),
   port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "cf4351f3b80262a4f196748fc49b9b91"),
+    },
+  },
 });
